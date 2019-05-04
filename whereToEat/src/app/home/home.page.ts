fcb3f99157b0eb0bd2d65c8721d40f000608ca47
Kristina Katovich <kristinakatovich@gmail.com>
2019-05-04 16:25:35 +0200

    added google map

--- whereToEat/src/app/home/home.page.ts
+++ whereToEat/src/app/home/home.page.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+
+declare var google: any;
+import {Geolocation} from '@ionic-native/geolocation/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterContentInit {
 
-  constructor() { }
+    @ViewChild('Map') mapElement: ElementRef;
+    map: any;
+    location = {lat: null, lng: null};
+    markerOptions: any = {position: null, map: null, title: null};
+    marker: any;
 
-  ngOnInit() {
-  }
+    /*Your API Key*/
+
+    constructor(public zone: NgZone, public geolocation: Geolocation) {
+
+    }
+
+    ngAfterContentInit() {
+
+    }
+
+    ngOnInit() {
+        this.loadMap();
+    }
+
+    loadMap() {
+        /*Get Current location*/
+        /*Map options*/
+        const mapOptions = {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 21,
+            mapTypeControl: google.maps.MapTypeId.ROADMAP
+        };
+        console.log('work ', mapOptions.center);
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+
+        /**
+         setTimeout(() => {
+
+        }, 3000);
+         **/
+    }
 
 }
